95ac04aecaadef0fe35c4b14870bba3cb7862eaf
Johnboi196 <68736696+Johnboi196@users.noreply.github.com>
2023-01-16 11:06:06 +0800

    Add files via upload

---
+++ tpplangat/config.php
@@ -0,0 +1,10 @@
+<?php
+$localhost = 'localhost';
+$username = 'root';
+$password = '';
+$database = 'datapelajar';
+$con = mysqli_connect($localhost, $username, $password, $database);
+if (!$con) {
+	echo "Connection failed!";
+}
+?>
